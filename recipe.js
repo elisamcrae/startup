b4d03574efbf3dd33bd7b65b87dcb3d11793833f
Elisa <elisamcrae@gmail.com>
2023-03-01 20:52:41 -0700

    fixed username issue

--- recipe.js
+++ recipe.js
@@ -1,16 +1,17 @@
 class User {
+    recipes;
+    //userName;
     constructor() {
-        recipes = [];
+        this.recipes = {name: "Mac and Cheese", image: "alt for now", recipe: "here's how to make it"};
         
-        userNameEl = document.querySelector('.user-name');
+        const userNameEl = document.querySelector('.user-name');
         userNameEl.textContent = this.getUserName();
+        //this.userName = userNameE1;
     }
 
     getUserName() {
         return localStorage.getItem('userName') ?? 'Unknown';
     }
 }
-
-
 
 const user = new User();
