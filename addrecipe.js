67f2c72dd91117bc6f88a41b04dbbd67aafc9389
Elisa <elisamcrae@gmail.com>
2023-03-02 11:01:28 -0700

    fixed errors in submit and get recipes

--- addrecipe.js
+++ addrecipe.js
@@ -1,10 +1,8 @@
 function addRecipe() {
     const recipeName = document.querySelector('#recipe_name');
     const recipeInstructions = document.querySelector('#recipe');
-    localStorage.setItem("RecipeName", recipeName);
-    localStorage.setItem("Recipe", recipeInstructions);
-    console.log("A");
+    localStorage.setItem("RecipeName", recipeName.value);
+    localStorage.setItem("Recipe", recipeInstructions.value);
     //this.recipes = Object.assign({3: {name: recipeName, image: "", recipe: recipeInstructions}}, this.recipes);
     window.location.replace("recipe.html");
-    console.log("B");
 }
--- recipe.js
+++ recipe.js
@@ -4,27 +4,34 @@ class User {
     constructor() {
         const userNameEl = document.querySelector('.user-name');
         userNameEl.textContent = this.getUserName();
+        this.getRecipe();
     }
 
     getUserName() {
+        //console.log(recipes.length);//TODELETE
         this.userName = localStorage.getItem('userName')
         if (this.userName) {
             return localStorage.getItem('userName') ?? 'Unknown';
         }
         else {
+            this.userName = "Unknown";
             return "Unknown"
         }
     }
 
-    addRecipe() {
-        const recipeName = document.querySelector('.recipe_name');
-        const recipeInstructions = document.querySelector('.recipe');
-        this.recipes = Object.assign({3: {name: recipeName, recipe: recipeInstructions}}, this.recipes);
+    getRecipe() {
+        let recipeName = localStorage.getItem('RecipeName');
+        let recipeInstructions = localStorage.getItem('Recipe');
+        if (recipeName && recipeInstructions) {
+            this.recipes.push([recipeName, recipeInstructions]);
+        }
+        console.log(recipes.length);//TODELETE
+        //this.recipes = Object.assign({3: {name: recipeName, recipe: recipeInstructions}}, this.recipes);
     }
 }
 
-class Recipes extends User() {
-    
-}
+/*class Recipes extends User() {
+    recip
+}*/
 
 const user = new User();
