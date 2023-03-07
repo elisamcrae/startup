93f167f559917929f0dd2774a3b02b66b6cf57ac
Elisa <elisamcrae@gmail.com>
2023-03-06 21:56:17 -0700

    updated printSuggestion

--- addrecipe.js
+++ addrecipe.js
@@ -1,18 +1,24 @@
 function addRecipe() {
     const recipeName = document.querySelector('#recipe_name');
     const recipeInstructions = document.querySelector('#recipe');
-    let rArray = [];
+    let rArrays = "";
     if (localStorage.getItem("Recipes")) {
         rArrays = localStorage.getItem("Recipes");
-        rArray.push(JSON.parse(rArrays));
-        rArray.push({"name": recipeName.value, "instructions": recipeInstructions.value});
+        rArrays += recipeName.value + "," + recipeInstructions.value + ",";
+        localStorage.setItem("Recipes", rArrays);
+        //rArray.push(JSON.parse(rArrays));
+        //console.log(rArray.length);
+        //console.log(rArray);
+        //rArray.push({"name": recipeName.value, "instructions": recipeInstructions.value});
         //const newA = {"name": recipeName.value, "instructions": recipeInstructions.value};
        // const joinedA = Object.assign(newA, rArray);
         //localStorage.setItem("Recipes", JSON.stringify(joinedA));
     } else {
-        rArray = {name: recipeName.value, instructions: recipeInstructions.value};
+        //rArrays = {name: recipeName.value, instructions: recipeInstructions.value};
+        //localStorage.setItem("Recipes", JSON.stringify(rArray));
+        toStr = recipeName.value + "," + recipeInstructions.value + ","
+        localStorage.setItem("Recipes", toStr)
     }
-    localStorage.setItem("Recipes", JSON.stringify(rArray));
     //localStorage.setItem("RecipeName", recipeName.value);
     //localStorage.setItem("Recipe", recipeInstructions.value);
     //this.recipes = Object.assign({3: {name: recipeName, image: "", recipe: recipeInstructions}}, this.recipes);
--- recipe.js
+++ recipe.js
@@ -4,6 +4,7 @@ class User {
     constructor() {
         const userNameEl = document.querySelector('.user-name');
         userNameEl.textContent = this.getUserName();
+        localStorage.clear()
         this.getRecipe();
         if (this.recipes.length === 0) {
             this.printSuggestion();
@@ -29,7 +30,7 @@ class User {
         //const recis = JSON.parse(localStorage.getItem("Recipes"));
         const recis = localStorage.getItem("Recipes");
         //if (this.recipes.includes(recis)) {
-        if (Object.values(this.recipes).includes(recis) === false) {
+        if (Object.values(this.recipes).includes(recis) === false && recis != null) {
             this.recipes.push(recis);
         }
         //const recis = JSON.parse(reci);
@@ -67,7 +68,7 @@ class User {
     }
 
     printRecipes() {
-        const matches = document.querySelectorAll('div.column1');
+        const matches = document.querySelectorAll('#column1');
         const onePrint = localStorage.getItem("Recipes");
         console.log(onePrint)//TO DEL
         const myA = onePrint.split(",")
@@ -85,7 +86,7 @@ class User {
         let i = 0;
         matches.forEach((userItem) => {
             if (i + 1< indices.length) {
-                userItem.textContent = myA[indices[i]] + "\r\n" + myA[indices[i+1]]
+                userItem.textContent = myA[indices[i]] + "\n" + myA[indices[i+1]]
                 ++i;
                 ++i;
             }
@@ -115,14 +116,15 @@ class User {
     }
 
     printSuggestion() {
-        const userNameEl = document.querySelector('.recipes');
+        const userNameEl = document.querySelector('#column1');
         const recInstr = "1. Bring a large pot of lightly salted water to a boil. Cook elbow macaroni in the boiling water, stirring occasionally until cooked through but firm to the bite, 8 minutes.\n"
         + "2. At the same time, melt butter in a saucepan over medium heat.\n"
         + "3. Add flour, salt, and pepper and stir until smooth, about 5 minutes.\n"
         + "4. Pour in milk slowly, while stirring continuously. Continue to cook and stir until mixture is smooth and bubbling, about 5 minutes, making sure the milk doesn't burn.\n"
         + "5. Add Cheddar cheese and stir until melted, 2 to 4 minutes.\n"
         + "6. Drain macaroni and fold into cheese sauce until coated.\n"
-        userNameEl.textContent = ["Mac and Cheese", recInstr];
+        userNameEl.textContent = "Mac and Cheese\n" + recInstr;
+        //userNameEl.textContent = recInstr;
     }
 }
 
